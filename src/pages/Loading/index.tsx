ba1b569e70cddc35c43fee75a5f1d87f8a4001c3
Hanna <hannaseraphim@gmail.com>
2025-11-25 11:37:26 -0400

    fix: center loading screen

--- src/pages/Loading/index.tsx
+++ src/pages/Loading/index.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "@/components/ui/shadcn-io/spinner";
 
 export const Loading = () => {
   return (
-    <div className="container flex w-dvw h-dvh">
+    <div className="flex w-dvw h-dvh items-center justify-center">
       <Spinner
         variant="ellipsis"
         className=" w-full h-dvh text-indigo-400 scale-10"
